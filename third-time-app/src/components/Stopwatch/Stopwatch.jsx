93a242a6ade09acf1fcb49a5348e422ed1b8dedb
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2026-08-12 16:37:21 -0400

    more styling, looking better now

--- third-time-app/src/components/Stopwatch/Stopwatch.jsx
+++ third-time-app/src/components/Stopwatch/Stopwatch.jsx
@@ -21,10 +21,9 @@ export default function Stopwatch() {
     <div className={styles.component}>
       <p>toggle here</p>
       <div className={styles.timebox}>
-        {hours > 0 && <span>{hours}:</span>}<span>{minutes < 10 && 0}{minutes}</span>:<span>{seconds < 10 && 0}{seconds}</span>
+        <span>{hours > 0 && <span>{hours}:</span>}<span>{minutes < 10 && 0}{minutes}</span>:<span>{seconds < 10 && 0}{seconds}</span></span>
       </div>
-      {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
-      {isRunning ? <button onClick={pause}>Pause</button> : <button onClick={start}>Start</button>}
+      {isRunning ? <button onClick={pause} className={styles.startPause}>Pause</button> : <button onClick={start} className={styles.startPause}>Start</button>}
       <button onClick={reset}>Reset</button>
     </div>
   );
