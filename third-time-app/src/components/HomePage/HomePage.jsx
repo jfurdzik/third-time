a3f587a68d6ab697cc7c54bb7cd1fb4a0af736b3
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2026-08-15 19:43:54 -0400

    timer main third logic working

--- third-time-app/src/components/HomePage/HomePage.jsx
+++ third-time-app/src/components/HomePage/HomePage.jsx
@@ -6,20 +6,30 @@ import { useState } from "react";
 import Timer from "../Timer/Timer";
 
 export default function HomePage() {
-    const [mode, setMode] = useState("");
+    const [mode, setMode] = useState("stopwatch");
     const handleCallback = (childData) => {
         setMode(childData);
     }
 
-    //change later, for now hardcoded to 10 mins timer
-    const time = new Date();
-    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+    //timer callback gets data from stopwatch child component
+    const [thirdSeconds, setThirdSeconds] = useState(0);
+    const handleCallbackTime = (childTime) => {
+        const third = childTime / 3; //this is a number (total seconds) not a date
+        setThirdSeconds(third);
+    }
+
+    //need this bc should capture current timestamp when switch not pause stamp (loses time when switching)
+    const generateCurrentExpiryTime = () => {
+        const finalTime = new Date();
+        finalTime.setSeconds(finalTime.getSeconds() + thirdSeconds); // add third time (number of secs) to current time Date obj
+        return finalTime;
+    }
 
     return(
         <>
             <img className={styles.logo} src={logo} height={100} width={100}/>
             <Toggle updateMode={handleCallback}></Toggle>
-            {mode === "stopwatch" ? <Stopwatch></Stopwatch> : <Timer expiryTimestamp={time}></Timer>}
+            {mode === "stopwatch" ? <Stopwatch sendTotalTime={handleCallbackTime}></Stopwatch> : <Timer expiryTimestamp={generateCurrentExpiryTime}></Timer>}
         </>
     );
 }
--- third-time-app/src/components/Stopwatch/Stopwatch.jsx
+++ third-time-app/src/components/Stopwatch/Stopwatch.jsx
@@ -3,7 +3,7 @@ import { useStopwatch } from 'react-timer-hook';
 import styles from './Stopwatch.module.css';
 import resetImg from '../../assets/reset.png';
 
-export default function Stopwatch() {
+export default function Stopwatch({ sendTotalTime }) {
   const {
     totalSeconds,
     milliseconds,
@@ -17,13 +17,18 @@ export default function Stopwatch() {
     reset,
   } = useStopwatch({ autoStart: false, interval: 20 });
 
+  const pauseAndSend = () => {
+    pause();
+    sendTotalTime(totalSeconds);
+  };
+
 
   return (
     <div className={styles.component}>
       <div className={styles.timebox}>
         <span>{hours > 0 && <span>{hours}:</span>}<span>{minutes < 10 && 0}{minutes}</span>:<span>{seconds < 10 && 0}{seconds}</span></span>
       </div>
-      {isRunning ? <button onClick={pause} className={styles.startPause}>pause</button> : <button onClick={start} className={styles.startPause}>start</button>}
+      {isRunning ? <button onClick={pauseAndSend} className={styles.startPause}>pause</button> : <button onClick={start} className={styles.startPause}>start</button>}
       <button className={styles.resetButton} onClick={() => reset(null, false)}><img src={resetImg} height={30} width={30}/></button>
     </div>
   );
